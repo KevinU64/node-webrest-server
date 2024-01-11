b525dea4b5e754373776b9c374362e08320d4107
Kevin Uruchi <uruchicoarite@gmail.com>
2024-01-11 18:16:27 -0400

    Clean architecture app

---
+++ src/domain/entities/todo.entity.ts
@@ -0,0 +1,33 @@
+
+
+
+export class TodoEntity {
+    
+    constructor(
+        public id: number,
+        public text: string,
+        public createdAt?: Date|null
+    ){}
+
+    get isCompleted() {
+        return !!this.createdAt;
+    }
+
+    public static fromObject( object: {[key: string]: any} ): TodoEntity {
+        const { id, text, createdAt } = object;
+        if ( !id ) throw 'Id is required';
+        if ( !text ) throw 'Text is required';
+
+        let newCreatedAt;
+        if ( createdAt ) {
+            newCreatedAt = new Date( createdAt );
+            if( isNaN( newCreatedAt.getTime() ) ) {
+                throw 'CompletedAt is not a valid date';
+            }
+        }
+
+        return new TodoEntity( id, text, createdAt );
+
+    }
+}
+
--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,69 +1,43 @@
 import { Request, Response } from "express";
 import { prisma } from "../../data/postgres";
 import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
+import { TodoRepository } from "../../domain";
 
-// const todos = [
-//     { id: 1, text: 'Kevin', createdAt: new Date() },
-//     { id: 2, text: 'Luis', createdAt: null },
-//     { id: 3, text: 'Lucas', createdAt: new Date() },
-// ];
+
 
 export class TodosController {
 
     //* DI
-    constructor() {}
+    constructor(
+        private readonly todoRepository: TodoRepository,
+    ) {}
 
 
     public getTodos = async(req: Request, res: Response) => {
-        //? POSTGRES
-        const allTodos = await prisma.todo.findMany()
-        return res.json(allTodos);
-
-        // return res.json(todos);
-    }
+        const todos = await this.todoRepository.getAll();
+        return res.json(todos);
+    };
 
     public getTodoById = async(req: Request, res: Response) => {
         const id = +req.params.id;
-        if ( isNaN(id) ) return res.status(400).json({ error: 'ID argument is not a number' });
-
-        //? POSTGRES
-        const todo = await prisma.todo.findUnique({
-            where: { id },
-        });
-
-        // const todo = todos.find( todo => todo.id === id);
-
-        ( todo )
-            ? res.json(todo)
-            : res.status(404).json({ error: `TODO with id ${ id } not found` });
+        
+        try {
+            const todo = await this.todoRepository.findById(id);
+            res.json(todo);
+        } catch (error) {
+            res.status(400).json({ error })
+        }
     
-    }
+    };
 
     public createTodo = async (req: Request, res: Response) => {
-        // const { text } = req.body;
-        // if ( !text ) return res.status( 400 ).json( { error: 'Text property is required' } );
-
+ 
         //* DTO
         const [error, createTodoDto] = CreateTodoDto.create(req.body);
         if ( error ) return res.status(400).json({  error });
 
-
-        //? POSTGRES
-        const todo = await prisma.todo.create({
-            data: createTodoDto!
-        })
-
+        const todo = await this.todoRepository.create( createTodoDto! );
         res.json( todo );
-
-        /*const newTodo = {
-            id: todos.length +1,
-            text: text,
-            createdAt: null,
-        };
-
-        todos.push(newTodo);
-
-        res.json(newTodo);*/
 
     }
 
@@ -72,66 +46,17 @@ export class TodosController {
         const [error, updateTodoDto] = UpdateTodoDto.create({...req.body, id});
         if ( error ) return res.status(400).json({ error });
         
-        //? POSTGRES
-        const todo = await prisma.todo.findUnique({
-            where: { id },
-        });
-
-        // const todo = todos.find( todo => todo.id === id);
-        if ( !todo ) return res.status(404).json({ error: 'Todo not found' });
+        const updatedTodo = await this.todoRepository.updateById( updateTodoDto! );
+        return res.json(updatedTodo);
         
-        // const { text, createdAt } = req.body; 
-        // if ( !text ) return res.status(400).json({ error: 'Text property is required' });
-
-        //? POSTGRES
-        const updatedTodo = await prisma.todo.update({
-            where: { id },
-            data: updateTodoDto!.values,
-        });
-
-        res.json( updatedTodo );
-
-        /*todo.text = text || todo.text;
-        ( createdAt === 'null' )
-            ? todo.createdAt = null
-            : todo.createdAt = new Date( createdAt || todo.createdAt );
-
-        todo.text = text;
-        ! OJO, referencia
-        ? Solucion 
-        todos.forEach( (todo, index) => {
-            if ( todo.id === id ){
-                todos[index] = todo;
-            }
-        });*/
-
-    }
+    };
 
     public delateTodo = async (req: Request, res: Response) => {
         const id = +req.params.id;
-        if ( isNaN(id) ) return res.status(400).json({ error: 'ID argument is not a number' });
-
-        //? POSTGRES
-        const todo = await prisma.todo.findUnique({
-            where: { id },
-        });
-
         
-        // const todo = todos.find(todo => todo.id === id);
-        if ( !todo ) return res.status(404).json({ error: `Todo with id ${ id } not found` });
-        
-        //? POSTGRES
-        const deletedTodo = await prisma.todo.delete({
-            where: { id }
-        });
+        const deletedTodo = await this.todoRepository.deleteById( id );
+        res.json(deletedTodo);
 
-        ( deletedTodo )
-            ? res.json( deletedTodo )
-            : res.status(404).json({ error: `Todo not exists` });
-
-        /* todos.splice( todos.indexOf(todo), 1 );
-        
-         res.json( {todo, deletedTodo} );*/
     }
 
 }
